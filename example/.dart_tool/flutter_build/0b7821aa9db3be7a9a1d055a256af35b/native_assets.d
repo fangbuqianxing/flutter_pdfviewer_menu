e2ad91820d2d8d1c82ea0d3466cf59da2a144fda
zhangyufei <yufei@tataufo.com>
2024-06-21 16:49:59 +0800

    修改yaml 文件的引用位置

---
+++ example/.dart_tool/flutter_build/0b7821aa9db3be7a9a1d055a256af35b/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/tataufo/Desktop/github/flutter_pdfviewer_menu/example/.dart_tool/flutter_build/0b7821aa9db3be7a9a1d055a256af35b/native_assets.yaml: 
---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tataufo/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tataufo/Desktop/github/flutter_pdfviewer_menu/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/tataufo/Desktop/github/flutter_pdfviewer_menu/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/tataufo/Desktop/github/flutter_pdfviewer_menu/example/.dart_tool/package_config.json"
